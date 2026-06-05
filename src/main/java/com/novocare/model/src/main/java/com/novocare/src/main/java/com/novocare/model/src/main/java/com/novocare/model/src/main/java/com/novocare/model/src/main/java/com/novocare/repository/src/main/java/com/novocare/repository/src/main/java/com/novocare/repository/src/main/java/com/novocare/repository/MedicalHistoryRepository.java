1b9122f1ecf1cb085d025c58112600d858f9f2da
min4521 <mintesnottizazu@gmail.com>
2026-06-06 01:02:22 +0300

    Create MedicalHistoryRepository.java

---
+++ src/main/java/com/novocare/model/src/main/java/com/novocare/src/main/java/com/novocare/model/src/main/java/com/novocare/model/src/main/java/com/novocare/model/src/main/java/com/novocare/repository/src/main/java/com/novocare/repository/src/main/java/com/novocare/repository/src/main/java/com/novocare/repository/MedicalHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.novocare.repository;
+
+import com.novocare.model.MedicalHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
+}
